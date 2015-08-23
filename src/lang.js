6da46bfc615149b61bbfb5ff5f46f754b57de76b
Alvin Dickson <dickson.alvin@gmail.com>
2015-08-23 15:20:15 +0000

    Solve issue #10 fallback methods
    Introduce setFallback/getFallback methods which will be used when the active language does not contain a given language line.

--- src/lang.js
+++ src/lang.js
@@ -27,7 +27,8 @@
     // Default options //
 
     var defaults = {
-        defaultLocale: 'en' /** The default locale if not set. */
+        defaultLocale: 'en', /** The default locale if not set. */
+        defaulFallbackLocale:'en'
     };
 
     // Constructor //
@@ -35,6 +36,8 @@
     var Lang = function(options) {
         options = options || {};
         this.defaultLocale = options.defaultLocale || defaults.defaultLocale;
+        this.defaulFallbackLocale = options.defaulFallbackLocale || defaults.defaulFallbackLocale;
+        
     };
 
     // Methods //
@@ -49,7 +52,26 @@
     Lang.prototype.setMessages = function(messages) {
         this.messages = messages;
     };
-
+    /**
+     * Set the fallback locale being used.
+     * 
+     * @param string fallback
+     * 
+     * @return void
+     */
+    Lang.prototype.setFallback = function(fallback){
+        this.defaulFallbackLocale = fallback;
+    }
+    
+    /**
+     * Get the fallback locale being used. 
+     * 
+     * @return void
+     */
+    Lang.prototype.getFallback = function () {
+        return this.defaulFallbackLocale;  
+    };
+    
     /**
      * Returns a translation message.
      *
@@ -182,9 +204,12 @@
         if (typeof key !== 'string') {
             return null;
         }
+        
         var segments = key.split('.');
+
         return {
             source: this.getLocale() + '.' + segments[0],
+            sourceFallback: this.getFallback() + '.' + segments[0],
             entries: segments.slice(1)
         };
     };
@@ -199,20 +224,22 @@
     Lang.prototype._getMessage = function(key) {
 
         key = this._parseKey(key);
-
+      
         // Ensure message source exists.
-        if (this.messages[key.source] === undefined) {
+        if (this.messages[key.source] === undefined && this.messages[key.sourceFallback] === undefined ) {
             return null;
         }
-
+           
         // Get message text.
-        var message = this.messages[key.source];
+     
+        var message = this.messages[key.source] || this.messages[key.sourceFallback];
+
         while (key.entries.length && (message = message[key.entries.shift()]));
 
         if (typeof message !== 'string') {
             return null;
         }
-
+     
         return message;
     };
 
---
+++ test/spec/lang_fallback_spec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var util = require('util');
+var Lang = require('../../src/lang.js');
+var messages = require('../fixture/messages');
+
+Lang.setMessages(messages);
+
+describe('The Lang\'s fallback methods', function() {
+
+    it('should exists', function() {
+        expect(Lang.setFallback).toBeDefined();
+        expect(Lang.getFallback).toBeDefined();
+
+    });
+
+    it('should be a function', function() {
+        expect(typeof Lang.setFallback).toBe('function');
+        expect(typeof Lang.getFallback).toBe('function');
+
+    });
+    
+    it('should return default fallback',function() {
+       expect(Lang.getFallback()).toBe('en'); 
+    });
+    it('should set default fallback',function() {
+        Lang.setFallback('fr'); 
+        expect(Lang.getFallback()).toBe('fr');
+    });
+    
+    it('should get the fallback expected message when locale not exists',function() {
+       Lang.setLocale('es');
+       Lang.setFallback('en');
+       expect(Lang.get('messages.home')).toBe('Inicio');
+       expect(Lang.get("reminders.sent")).toBe('Password reminder sent!');
+       Lang.setLocale('en');
+    });
+
+
+});

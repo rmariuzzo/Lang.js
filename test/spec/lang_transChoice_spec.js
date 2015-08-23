2575967fdc93e5916f23ca0d877d2677c8792989
Rubens Mariuzzo <rubens@mariuzzo.com>
2015-08-23 16:17:25 -0400

    Added more tests to `transChoice()` method.

--- test/spec/lang_transChoice_spec.js
+++ test/spec/lang_transChoice_spec.js
@@ -6,10 +6,33 @@ var messages = require('../fixture/messages');
 
 Lang.setMessages(messages);
 
-describe('The Lang.transChoice() method', function() {
+describe('The Lang.transChoice() method', function () {
 
-    it('should exists', function() {
+    it('should exists', function () {
         expect(Lang.transChoice).toBeDefined();
+    });
+
+    it('should be a function', function () {
+        expect(typeof Lang.transChoice).toBe('function');
+    });
+
+    it('should return the passed key when not found', function () {
+        expect(Lang.transChoice('foo.bar', 1)).toBe(Lang.choice('foo.bar', 1));
+        expect(Lang.transChoice(null, 1)).toBe(Lang.choice(null, 1));
+    });
+
+    it('should return the expected message', function () {
+        expect(Lang.transChoice('messages.plural', 1)).toBe(Lang.choice('messages.plural', 1));
+        expect(Lang.transChoice('messages.plural', 10)).toBe(Lang.choice('messages.plural', 10));
+    });
+
+    it('should return the expected message with replacements', function () {
+        expect(Lang.transChoice('validation.accepted', 1)).toBe(Lang.choice('validation.accepted', 1));
+        expect(Lang.transChoice('validation.accepted', 1, {
+            'attribute': 'foo'
+        })).toBe(Lang.choice('validation.accepted', 1, {
+            'attribute': 'foo'
+        }));
     });
 
 });

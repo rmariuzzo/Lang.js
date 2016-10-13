8ae7d194c2e3ff10ceb7389292f5bd5fce12755a
Larry Philps <larry@philps.ca>
2016-10-13 16:41:57 -0400

    Enable more than 1 level of nested directories
    in _parseKey, the replace function should replace all occurrences of /
    with ., not just the first occurrence.

--- test/spec/lang_trans_spec.js
+++ test/spec/lang_trans_spec.js
@@ -39,6 +39,10 @@ describe('The lang.trans() method', function () {
         expect(lang.trans('forum/thread.viewAllThreads')).toBe('View all threads');
     });
 
+    it('should return the expected message inside a deeply nested directory', function () {
+        expect(lang.trans('dir1/dir2/child.deeplynestedmessage')).toBe('Message from file 2 directories down');
+    });
+
     it('should return the passed key when nested message does not point to a message', function () {
         expect(lang.trans('messages.family.children')).toBe('messages.family.children');
         expect(lang.trans('a.b.c.d.f.g.h.i.j.k')).toBe('a.b.c.d.f.g.h.i.j.k');

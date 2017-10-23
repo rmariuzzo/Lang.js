e89adc08c73fa2f40967bd2c7241fb7df37c1dbd
Renan GEHAN <rgehan94@gmail.com>
2017-10-23 21:40:04 +0200

    Add test for nested dotted key

--- test/spec/lang_get_spec.js
+++ test/spec/lang_get_spec.js
@@ -68,4 +68,8 @@ describe('The lang.get() method', function () {
     it('should return the expected message if the key has a dot', function() {
         expect(lang.get('messages.dot.in.key')).toBe('Dot In Key');
     })
+
+    it('should return the expected message if the key is nested and has a dot', function() {
+        expect(lang.get('messages.category.dot.in.key')).toBe('Dot In Key');
+    })
 });

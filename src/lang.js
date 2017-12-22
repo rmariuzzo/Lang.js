31474a87dc21e4434b66b527d00944c61fc2a13d
Rubens Mariuzzo <rubens@mariuzzo.com>
2017-12-22 12:37:20 -0400

    v1.1.12.

--- src/lang.js
+++ src/lang.js
@@ -1,7 +1,7 @@
 /*!
  *  Lang.js for Laravel localization in JavaScript.
  *
- *  @version 1.1.11
+ *  @version 1.1.12
  *  @license MIT https://github.com/rmariuzzo/Lang.js/blob/master/LICENSE
  *  @site    https://github.com/rmariuzzo/Lang.js
  *  @author  Rubens Mariuzzo <rubens@mariuzzo.com>

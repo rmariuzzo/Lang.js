4311f1af8d35646ca8234c1b7ba892abfb6b57e9
Rubens Mariuzzo <rubens@mariuzzo.com>
2019-02-09 10:40:44 -0400

    Update Jest config.

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,8 @@
 module.exports = {
-"roots": [
-  "<rootDir>/test"
-  ],
-  "transform": {
+  roots: ["<rootDir>/test"],
+  transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-}
+  testRegex: ".*.test.ts$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+};
